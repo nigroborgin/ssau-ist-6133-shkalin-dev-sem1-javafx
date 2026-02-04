3811259fe110a70d450a2e59dc8798751779337c
Vyacheslav Shkalin <vyacheslav.shkalin@moneta.ru>
2026-02-04 16:01:05 +0400

    Initital commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ssau-ist-6133-shkalin-dev-sem1
+## Lab 5. JavaFX
---
+++ src/ru/ssau/vyashka/labs/javafx/Main.java
@@ -0,0 +1,9 @@
+package ru.ssau.vyashka.labs.javafx;
+
+public class Main {
+
+    static void main() {
+        System.out.println("lab-5 javafx");
+    }
+
+}
